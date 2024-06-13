eaf78f686927218b57f136f9646d55122fe58697
yewl1110 <38202551+yewl1110@users.noreply.github.com>
2024-06-13 16:38:16 +0900

    feat(client): AddRuleDialog useform, yup 적용

--- client/src/components/AddRuleDialog.jsx
+++ client/src/components/AddRuleDialog.jsx
@@ -11,10 +11,41 @@ import {
 } from '@mui/material'
 import Grid2 from '@mui/material/Unstable_Grid2'
 import SmallDateTimePicker from './SmallDateTimePicker'
+import { useYupValidationResolver } from '../hooks/useYupValidationResolver'
+import { ruleSchema } from '../schema/schema'
+import { useForm } from 'react-hook-form'
+import dayjs from 'dayjs'
 
 export default function AddRuleDialog({ close }) {
+  const resolver = useYupValidationResolver(ruleSchema)
+  const {
+    handleSubmit,
+    register,
+    watch,
+    formState: { errors },
+    setValue,
+  } = useForm({ resolver })
+
+  const submit = (data) => {
+    console.log(data)
+  }
+
+  const AddDayButton = ({ day }) => {
+    return (
+      <IconButton
+        size="small"
+        onClick={() => {
+          const start = dayjs(watch('period.start'))
+          setValue('period.end', start.add(day, 'day'))
+        }}
+      >
+        {`+${day}D`}
+      </IconButton>
+    )
+  }
+
   return (
-    <Dialog open={true}>
+    <Dialog open={true} maxWidth={'xs'}>
       <DialogTitle>규칙 추가</DialogTitle>
       <IconButton
         aria-label="close"
@@ -29,28 +60,72 @@ export default function AddRuleDialog({ close }) {
         <Icon>close</Icon>
       </IconButton>
       <DialogContent dividers>
-        <Grid2 container spacing={2}>
-          <Grid2 xs={6}>
+        <Grid2 container spacing={2} columns={1}>
+          <Grid2 xs={1}>
             <Box>
-              <TextField label="차단 할 IP 주소" size="small" fullWidth />
+              <TextField
+                label="차단 할 IP 주소 ex) 100.10.1.1"
+                size="small"
+                error={!!errors['ip']}
+                helperText={errors['ip']?.message}
+                fullWidth
+                max={15}
+                inputProps={{ ...register('ip'), maxLength: 15 }}
+              />
             </Box>
           </Grid2>
-          <Grid2 xs={12}>
+          <Grid2 xs={1}>
             <Box>
-              <TextField label="설명 (최대 20자)" size="small" fullWidth />
+              <TextField
+                label="설명 (최대 20자)"
+                size="small"
+                error={!!errors['memo']}
+                helperText={errors['memo']?.message}
+                fullWidth
+                inputProps={{ ...register('memo'), maxLength: 20 }}
+                onBlur={(event) => {
+                  event.target.value = event.target.value.trim()
+                }}
+              />
             </Box>
           </Grid2>
-          <Grid2 xs={6}>
-            <SmallDateTimePicker label="차단 시작 날짜" />
+          <Grid2 xs={1}>
+            <SmallDateTimePicker
+              label="차단 시작 날짜"
+              error={!!errors['period.start']}
+              helperText={errors['period.start']?.message}
+              setValue={(value) => {
+                setValue('period.start', value)
+              }}
+              value={
+                (watch('period.start') && dayjs(watch('period.start'))) || null
+              }
+            />
+            {watch('period.start') && (
+              <>
+                <AddDayButton day={1} />
+                <AddDayButton day={3} />
+                <AddDayButton day={7} />
+              </>
+            )}
           </Grid2>
-          <Grid2 xs={6}></Grid2>
-          <Grid2 xs={6}>
-            <SmallDateTimePicker label="차단 해제 날짜" />
+          <Grid2 xs={1}>
+            <SmallDateTimePicker
+              label="차단 해제 날짜"
+              error={!!errors['period.end']}
+              helperText={errors['period.end']?.message}
+              setValue={(value) => {
+                setValue('period.end', value)
+              }}
+              value={
+                (watch('period.end') && dayjs(watch('period.end'))) || null
+              }
+            />
           </Grid2>
         </Grid2>
       </DialogContent>
       <DialogActions>
-        <Button>추가</Button>
+        <Button onClick={handleSubmit(submit)}>추가</Button>
       </DialogActions>
     </Dialog>
   )
---
+++ client/src/schema/schema.js
@@ -0,0 +1,37 @@
+import { object, string } from 'yup'
+
+export const ruleSchema = object({
+  ip: string()
+    .matches(
+      /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/,
+      '올바른 IP 형식이 아닙니다.'
+    )
+    .required('필수 항목입니다.'),
+  memo: string().max(20).required('필수 항목입니다.'),
+  period: object({
+    start: string()
+      .required('필수 항목입니다.')
+      .test({
+        test: (value, ctx) => {
+          if (!!ctx.parent.end && value >= ctx.parent.end) {
+            return ctx.createError({
+              message: '차단 시작 날짜는 해제 날짜보다 빨라야 합니다.',
+            })
+          }
+          return true
+        },
+      }),
+    end: string()
+      .required('필수 항목입니다.')
+      .test({
+        test: (value, ctx) => {
+          if (!!ctx.parent.start && ctx.parent.start >= value) {
+            return ctx.createError({
+              message: '차단 해제 날짜는 시작 날짜보다 늦어야 합니다.',
+            })
+          }
+          return true
+        },
+      }),
+  }),
+})
